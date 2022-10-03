8292432cd21be464abcb7139a6064992783aac70
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-10-03 16:46:46 -0400

    why the fuck are you reinsatlling this shit

--- binaries/postboot.sh
+++ binaries/postboot.sh
@@ -1,6 +1,4 @@
 mount -uw /
 sleep 1
-curl -sLO https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.2.3_iphoneos-arm.deb
-curl -sLO https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
-dpkg -i *.deb
+/etc/rc.d/*
 sbreload
