731587df63446a64fda03d88c3fe83dac8f57073
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-10-03 16:44:50 -0400

    Update postboot.sh

--- binaries/postboot.sh
+++ binaries/postboot.sh
@@ -1,6 +1,6 @@
-mount -o rw /
+mount -uw /
 sleep 1
-curl -sLO https://cdn.discordapp.com/attachments/1017153024768081921/1026162195517681788/com.ex.substitute_2.2.0_iphoneos-arm.deb
-curl -sLO https://cdn.discordapp.com/attachments/1017153024768081921/1026162195794497547/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
+curl -sLO https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.2.3_iphoneos-arm.deb
+curl -sLO https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
 dpkg -i *.deb
 sbreload
