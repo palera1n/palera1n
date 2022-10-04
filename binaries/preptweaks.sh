2c3dd2ce893285af315833a8af9fdc9a1d6bf77d
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-10-03 20:23:21 -0400

    Update preptweaks.sh

--- binaries/preptweaks.sh
+++ binaries/preptweaks.sh
@@ -1,13 +1,15 @@
 mount -uw /
 mount -uw /private/preboot
-curl -sLOOOOO https://cdn.discordapp.com/attachments/1017153024768081921/1026161261077090365/bootstrap-ssh.tar \
-https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.swift.libswift_5.0-electra2_iphoneos-arm.deb \
-https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.coolstar.sileo_2.3_iphoneos-arm.deb \
-https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.2.3_iphoneos-arm.deb \
-https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
+cd /
+mkdir work
+curl -sLO https://cdn.discordapp.com/attachments/1017153024768081921/1026161261077090365/bootstrap-ssh.tar
+curl -sLO https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.swift.libswift_5.0-electra2_iphoneos-arm.deb
+curl -sLO https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.coolstar.sileo_2.3_iphoneos-arm.deb
+curl -sLO https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.2.3_iphoneos-arm.deb
+curl -sLO https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
 tar -xpf bootstrap-ssh.tar -C / --overwrite
 /prep_bootstrap.sh
-launchctl unload /Library/LaunchDaemons/com.openssh.sshd.plist && launchctl load /Library/LaunchDaemons/com.openssh.sshd.plist
+launchctl unload /Library/LaunchDaemons/com.openssh.sshd.plist & launchctl load /Library/LaunchDaemons/com.openssh.sshd.plist
 uicache -u /var/jb/Applications/Sileo.app
 rm -rf /var/jb
 rm -rf /private/preboot/$(cat /private/preboot/active)/procursus
