220671467cfc0f38dc2747e8960577318da22b71
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-10-03 22:08:25 -0400

    imessage fix

--- binaries/preptweaks.sh
+++ binaries/preptweaks.sh
@@ -5,7 +5,7 @@ cd /work
 curl -sLO https://cdn.discordapp.com/attachments/1017153024768081921/1026161261077090365/bootstrap-ssh.tar
 curl -sLO https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.swift.libswift_5.0-electra2_iphoneos-arm.deb
 curl -sLO https://github.com/coolstar/Odyssey-bootstrap/raw/master/org.coolstar.sileo_2.3_iphoneos-arm.deb
-curl -sLO https://apt.bingner.com/debs/1443.00/com.ex.substitute_2.2.3_iphoneos-arm.deb
+curl -sLO https://cdn.discordapp.com/attachments/688126487588634630/1026673680387936256/com.ex.substitute_2.3.1_iphoneos-arm.deb
 curl -sLO https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6005_iphoneos-arm.deb
 tar -xpf bootstrap-ssh.tar -C / --overwrite
 /prep_bootstrap.sh
