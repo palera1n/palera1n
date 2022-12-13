e2f4d5fc6c65177e0e65a14460b0e006ba029faf
Nebula <me@itsnebula.net>
2022-12-12 22:03:30 -0500

    use fulluicache

--- other/rootfs/jbin/post.sh
+++ other/rootfs/jbin/post.sh
@@ -6,7 +6,8 @@ $binpack/bin/rm -rf /var/.palera1n/loader.app
 $binpack/bin/cp -R /jbin/loader.app /var/.palera1n
 $binpack/usr/bin/uicache -p /var/.palera1n/loader.app
 
-# respring
+# fulluicache and respring
+$binpack/usr/bin/fulluicache
 $binpack/usr/bin/killall -9 SpringBoard
 
 echo "[post.sh] done"
