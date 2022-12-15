fb326d43594dda1d8e199059cf445cef19d23893
Nebula <me@itsnebula.net>
2022-12-15 18:31:27 -0500

    dev mode patch (actually this time)

--- palera1n.sh
+++ palera1n.sh
@@ -690,7 +690,7 @@ if [ ! -f blobs/"$deviceid"-"$version".der ]; then
     remote_cp root@localhost:/mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kcache.patched work/
     if [ "$tweaks" = "1" ]; then
         if [[ "$version" == *"16"* ]]; then
-            "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e -o -u -l -t -h
+            "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e -o -u -l -t -h -d
         else
             "$dir"/Kernel64Patcher work/kcache.patched work/kcache.patched2 -e -l
         fi
