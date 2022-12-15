9e92dc604ae02ec7eb0a5801ee0647ce06aa444f
Nebula <me@itsnebula.net>
2022-12-14 19:11:52 -0500

    straight up uicache it from jbin

--- other/rootfs/jbin/post.sh
+++ other/rootfs/jbin/post.sh
@@ -1,10 +1,8 @@
 binpack=/jbin/binpack
 
 # uicache loader app
-$binpack/bin/mkdir -p /var/.palera1n
 $binpack/bin/rm -rf /var/.palera1n/loader.app
-$binpack/bin/cp -R /jbin/loader.app /var/.palera1n
-$binpack/usr/bin/uicache -p /var/.palera1n/loader.app
+$binpack/usr/bin/uicache -p /jbin/loader.app
 
 # respring
 $binpack/usr/bin/killall -9 SpringBoard
