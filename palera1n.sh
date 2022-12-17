2c49e436b1188d8d338596e4b3bf5ffa87eb53ed
Nebula <me@itsnebula.net>
2022-12-17 18:00:29 -0500

    cant believe that was the issue

--- palera1n.sh
+++ palera1n.sh
@@ -565,10 +565,10 @@ if [ ! -f blobs/"$deviceid"-"$version".der ]; then
     
     if [ "$tweaks" = "1" ]; then
         echo "[*] Testing for baseband presence"
-        if [ "$(remote_cmd "/usr/bin/mgask HasBaseband | grep -E 'true|false'")" = "true" ] && [ "${cpid}" == *"0x700"* ]; then
+        if [ "$(remote_cmd "/usr/bin/mgask HasBaseband | grep -E 'true|false'")" = "true" ] && [[ "${cpid}" == *"0x700"* ]]; then
             disk=7
         elif [ "$(remote_cmd "/usr/bin/mgask HasBaseband | grep -E 'true|false'")" = "false" ]; then
-            if [ "${cpid}" == *"0x700"* ]; then
+            if [[ "${cpid}" == *"0x700"* ]]; then
                 disk=6
             else
                 disk=7
