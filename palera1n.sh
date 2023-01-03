b4ba4de2dc544b75b05f5670cbe4547a13aa6acd
junepark678 <40409848+junepark678@users.noreply.github.com>
2023-01-03 23:20:11 +0900

    Make palera1n.sh not directory dependant (#308)
    * Make $dir not working directory dependent
    
    * Change all occurrences of $(pwd) and change git
    
    $(pwd) -> $(dirname "$0")
    git -> git -C $(dirname "$0")
    
    * Update palera1n.sh

--- palera1n.sh
+++ palera1n.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pushd $(dirname "$0")
+
 mkdir -p logs
 set -e 
 
@@ -1011,3 +1013,5 @@ echo "If you have any issues, please join the Discord server and ask for help: h
 echo "Enjoy!"
 
 } 2>&1 | tee logs/${log}
+
+popd
