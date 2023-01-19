b42e6309683c263238b69e6107c030de63c8a778
Oliver Tzeng <olivertzeng@proton.me>
2023-01-19 14:14:43 +0800

    l

--- palera1n.sh
+++ palera1n.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-MODE=$(gum table < others/modes.csv | cut -d ',' -f 1)
+MODE=$(gum table < modes.csv | cut -d ',' -f 1)
 IOS=$(gum input --placeholder 'iOS version please... (optional)')
 echo 'Other flags?'
-FLAGS=$(gum choose --no-limit < other/flags.md)
+FLAGS=$(gum choose --no-limit < flags.md)
 SHORT_FLAGS=$(echo $FLAGS | cut -d ' ' -f2 | tr -d '()')
 ./main.sh $IOS $MODE $SHORT_FLAGS
