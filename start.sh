8d27d944b121df2d5b58bcd6e267cad9776048c1
Oliver Tzeng <olivertzeng@proton.me>
2023-01-19 14:28:43 +0800

    original iglkjlkj;klj

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+MODE=$(gum table < modes.csv | cut -d ',' -f 1)
+IOS=$(gum input --placeholder 'iOS version please... (optional)')
+echo 'Other flags?'
+FLAGS=$(gum choose --no-limit < flags.md)
+SHORT_FLAGS=$(echo $FLAGS | cut -d ' ' -f2 | tr -d '()')
+./main.sh $IOS $MODE $SHORT_FLAGS
