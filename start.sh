3d12690744f0648fd232dfe6c713b8e774c6e188
Oliver Tzeng <olivertzeng@proton.me>
2023-01-23 16:03:14 +0800

    Make tons of files move into the gum folder

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export LC_CTYPE="en_US.UTF-8"
-MODE=$(gum table < modes.csv | cut -d ',' -f 1)
+MODE=$(gum table < gum/modes.csv | cut -d ',' -f 1)
 IOS=$(gum input --placeholder 'optional' --prompt.foreground "212" --prompt "Whats your iOS version? > ")
 echo 'Other flags?'
-FLAGS=$(gum choose --no-limit < flags.md)
+FLAGS=$(gum choose --no-limit < gum/flags.md)
 SHORT_FLAGS=$(echo $FLAGS | cut -d ' ' -f2 | tr -d '()')
 ./palera1n.sh $IOS $MODE $SHORT_FLAGS
