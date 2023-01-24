8caf8c029ac2fe3b3378d1245c2aad9e8c1b367d
Oliver Tzeng <olivertzeng@proton.me>
2023-01-24 09:54:17 +0800

    make those gum tables and styles look better

--- palera1n.sh
+++ palera1n.sh
@@ -454,7 +454,7 @@ if [ "$tweaks" = 1 ] && [ ! -e ".tweaksinstalled" ] && [ ! -e ".disclaimeragree"
 	answer=$(gum input --placeholder 'Yes, do as I say')
 	if [ "$answer" = 'Yes, do as I say' ]; then
         gum style \
-            --foreground 9 --border-foreground 9 --border double \
+            --foreground 9 --border-foreground 9 --border thick\
             --align center --width 50 --margin "1 2" --padding "2 4" \
             "Are you REALLY sure? WE WARNED YOU" "Type $(gum style --foreground 1 'Yes, I am sure')" "to continue"
 		answer=$(gum input --placeholder 'Yes, I am sure')
@@ -888,7 +888,7 @@ DownloadBinpack () {
     sleep 2
     clear
     gum style \
-        --foreground 212 --border-foreground 212 --border double \
+        --foreground 212 --border-foreground 212 --border normal\
         --align center --width 50 --margin "1 2" --padding "2 4" \
         "Phase 1 done! Rebooting your device" "If it doesn't reboot, you may force reboot"
     remote_cmd "/sbin/reboot"
@@ -1089,12 +1089,12 @@ rm -rf work rdwork
 
 # Displaying gum with some blocks
 clear
-DONE=$(gum style --height 5 --width 25 --padding '1 3' --border double --border-foreground 57  "Done!" "The device should now boot to $(gum style --foreground 212 "iOS")")
-UNLOCK=$(gum style --width 25 --padding '1 3' --border double --border-foreground 212 "When you unlock the device, it will respring about $(gum style --foreground "#04B575" "30 seconds") later.")
-FIRST=$(gum style  --height 5 --width 35 --padding '1 8' --border double --border-foreground 255 "If this is your first time jailbreaking," "open the new palera1n app, then press $(gum style --foreground 57 "Install").")
-ISSUE=$(gum style  --height 7 --width 35 --padding '1 5' --border double --border-foreground 120  "If you have any issues, please first check the $(gum style --foreground 212 "common-issues.md") document for common issues")
+DONE=$(gum style --height 5 --width 25 --padding '1 3' --border rounded --border-foreground 57  "Done!" "The device should now boot to $(gum style --foreground 212 "iOS")")
+UNLOCK=$(gum style --width 25 --padding '1 3' --border rounded --border-foreground 212 "When you unlock the device, it will respring about $(gum style --foreground "#04B575" "30 seconds") later.")
+FIRST=$(gum style  --height 5 --width 35 --padding '1 8' --border normal --border-foreground 255 "If this is your first time jailbreaking," "open the new palera1n app, then press $(gum style --foreground 57 "Install").")
+ISSUE=$(gum style  --height 7 --width 35 --padding '1 5' --border normal --border-foreground 120  "If you have any issues, please first check the $(gum style --foreground 212 "common-issues.md") document for common issues")
 DISCORD=$(gum style  --height 7 --width 35 --padding '1 5' --border double --border-foreground 120  "If that list doesn't solve your issue, join the $(gum style --foreground 212 "Discord") server" "and ask for help:" "$(gum style --foreground 212 "https://dsc.gg/palera1n")")
-ENJOY=$(gum style --width 15 --align center --padding "1 1" --border double --border-foreground 57 $(gum style --foreground 212 "Enjoy!"))
+ENJOY=$(gum style --width 15 --align center --padding "1 1" --border thick --border-foreground 57 $(gum style --foreground 212 "Enjoy!"))
 
 DONE_UNLOCK=$(gum join "$DONE" "$UNLOCK")
 FIRST_ISSUE=$(gum join "$FIRST" "$ISSUE")
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export LC_CTYPE="en_US.UTF-8"
-MODE=$(gum table < gum/modes.csv | cut -d ',' -f 1)
+MODE=$( gum table < gum/modes.csv -w 24,14 --height="15" --cell.border="rounded" | cut -d ',' -f 1)
 IOS=$(gum input --placeholder 'optional' --prompt.foreground "212" --prompt "Whats your iOS version? > ")
 echo 'Other flags?'
 FLAGS=$(gum choose --no-limit < gum/flags.md)
