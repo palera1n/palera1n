d79bef4cfd289a64cfdd56a7b99667ba82dca53f
Oliver Tzeng <olivertzeng@proton.me>
2023-01-26 17:20:15 +0800

    shorten some useless things

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export LC_CTYPE="en_US.UTF-8"
-MODE=$( gum table < gum/modes.csv -w 24,14 --height="15" --cell.border="rounded" | cut -d ',' -f 1)
+MODE=$(gum table -f gum/modes.csv -w 24,14 --height="15" --cell.border="rounded" --cell.foreground="222" --header.foreground='222' | cut -d ',' -f 1)
 IOS=$(gum input --placeholder 'optional' --prompt.foreground "212" --prompt "Whats your iOS version? > ")
 echo 'Other flags?'
 FLAGS=$(gum choose --no-limit < gum/flags.md)
