3b83682825ff96eca01c23b175c16555c88a76c2
Oliver Tzeng <olivertzeng@proton.me>
2023-02-01 19:51:20 +0800

    cut sucks

--- gum/flags.md
+++ gum/flags.md
@@ -1,5 +1,5 @@
-Skip FakeFS (--skip-fakefs)
-No Baseband (--no-baseband)
-Debug (--debug)
-Serial (--serial)
-NoFlags ( )
+Skip FakeFS (--skip-fakefs) Don't create the fakefs even if --semi-tethered is specified
+No Baseband (--no-baseband) Indicate that the device does not have a baseband 
+Print Debug (--debug) Debug the script 
+Serial Output (--serial) Enable serial output on the device (only needed for testing with a serial cable)
+No Flags ( )
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 export LC_CTYPE="en_US.UTF-8"
-MODE=$(gum table -f gum/modes.csv -w 24,14 --height="15" --cell.border="rounded" --cell.foreground="222" --header.foreground='222' | cut -d ',' -f 1)
+MODE=$(gum table -f gum/modes.csv -w 24,23,65 --height="15" --header.foreground='222' --cell.border='rounded' --cell.foreground='222' | cut -d ',' -f 1)
 IOS=$(gum input --placeholder 'optional' --prompt.foreground "212" --prompt "Whats your iOS version? > ")
 echo 'Other flags?'
-FLAGS=$(gum choose --no-limit < gum/flags.md)
-SHORT_FLAGS=$(echo $FLAGS | cut -d ' ' -f2 | tr -d '()')
-./palera1n.sh $IOS $MODE $SHORT_FLAGS
+FLAGS=$(gum choose --no-limit --item.foreground='222' < gum/flags.md | cut -d ' ' -f3 | tr -d '()')
+./palera1n.sh $IOS $MODE $FLAGS
