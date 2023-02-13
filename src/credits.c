f00cac767291dd56bca5efe12c3cdb6d4ac72395
Nick Chan <towinchenmi@gmail.com>
2023-02-13 10:46:33 +0800

    credit changes

--- src/credits.c
+++ src/credits.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-#define EE fprintf
-#define Q }
-#define P print_credits() {
+#ifndef BAKERAIN_DEVELOPER
 #define BAKERAIN_DEVELOPER "dora2ios"
-#define O void
-#define Y )
-#define Z (
-#define R stdout
-#define S ,
-#define T ;
-#define A "# == palera1n-c == \n#\n"
-#define B "# Made by: Nick Chan, Tom, Mineek, Nebula, llsc12\n#\n"
-#define C "# Thanks to: " BAKERAIN_DEVELOPER ", pythonplayer, tihmstar, nikias\n"
-#define D "# (libimobiledevice), checkra1n team (Siguza, axi0mx, littlelailo\n"
-#define E "# et al.), Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n\n"
+#endif
 
-O P EE Z R S A B C D E Y T Q
+#define T void
+#define R print_credits() {
+#define O fprintf
+#define U (
+#define B stdout
+#define L ,
+#define E "# == palera1n-c == \n#\n"
+#define M "# Made by: Nick Chan, Tom, Mineek, Nebula, llsc12\n#\n"
+#define A "# Thanks to: " BAKERAIN_DEVELOPER ", pythonplayer, tihmstar, nikias\n"
+#define K "# (libimobiledevice), checkra1n team (Siguza, axi0mx, littlelailo\n"
+#define I "# et al.), Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n\n"
+#define N )
+#define G ;
+#define S }
+
+T R O U B L E M A K I N G S
