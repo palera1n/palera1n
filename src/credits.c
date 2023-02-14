87dae1a7e2cdcb4a23ddff163a144e6b165aab02
Tom E <guacaplushy@icloud.com>
2023-02-14 07:04:48 -0500

    the plush

--- src/credits.c
+++ src/credits.c
@@ -3,7 +3,7 @@
 #ifndef BAKERAIN_DEVELOPE_R
 #define BAKERAIN_DEVELOPE_R "dora2ios"
 #endif
-#define THE_PLUSH "Tom"
+#define THE_PLUSH "Ploosh"
 
 #define T void
 #define R print_credits() {
@@ -12,7 +12,7 @@
 #define B stdout
 #define L ,
 #define E "# == palera1n-c == \n#\n"
-#define M "# Made by: Nick Chan, " THE_PLUSH " , Mineek, Nebula, llsc12\n#\n"
+#define M "# Made by: Nick Chan, " THE_PLUSH ", Mineek, Nebula, llsc12\n#\n"
 #define A "# Thanks to: " BAKERAIN_DEVELOPE_R ", pythonplayer, tihmstar, nikias\n"
 #define K "# (libimobiledevice), checkra1n team (Siguza, axi0mx, littlelailo\n"
 #define I "# et al.), Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n\n"
