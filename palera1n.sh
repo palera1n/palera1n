74878f9063241873696d0fc5c738b53a048e115b
alexia <me@alexia.lol>
2023-03-15 18:14:27 +0100

    Add palera1n.sh stub telling people about migration

---
+++ palera1n.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "[!] palera1n has received a major rewrite. Please refer to <https://docs.palera.in/usage/migration-from-palera1n.sh>."
+echo "[!] If you would like to continue using the legacy version, please run the following command: git checkout legacy"
