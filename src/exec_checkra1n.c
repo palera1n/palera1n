74ebacf120fd3a43c5388f03bff276e27969396e
Nick Chan <towinchenmi@gmail.com>
2023-11-23 12:18:30 +0800

    fix embedded resources w/ 16-bit int

--- src/exec_checkra1n.c
+++ src/exec_checkra1n.c
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <inttypes.h>
 #include <sys/utsname.h>
 
 #if defined(__APPLE__)
@@ -51,7 +52,7 @@ int exec_checkra1n(void) {
 		}
 		ssize_t didWrite_Pongo_bin = write(fd_pongo, Pongo_bin, Pongo_bin_len);
 		if (didWrite_Pongo_bin != (ssize_t)Pongo_bin_len) {
-			LOG(LOG_FATAL, "Size written to %s does not match expected: %zd != %u: %d (%s)", pongo_path, didWrite_Pongo_bin, Pongo_bin_len, errno, strerror(errno));
+			LOG(LOG_FATAL, "Size written to %s does not match expected: %zd != %" PRIu32 ": %d (%s)", pongo_path, didWrite_Pongo_bin, Pongo_bin_len, errno, strerror(errno));
 			close(fd_pongo);
 			unlink(pongo_path);
 			free(pongo_path);
@@ -71,7 +72,7 @@ int exec_checkra1n(void) {
 	}
 	ssize_t didWrite_checkra1n = write(fd_checkra1n, checkra1n, checkra1n_len);
 	if (didWrite_checkra1n != (ssize_t)checkra1n_len) {
-		LOG(LOG_FATAL, "Size written to %s does not match expected: %zd != %d: %d (%s)", checkra1n_path, didWrite_checkra1n, checkra1n_len, errno, strerror(errno));
+		LOG(LOG_FATAL, "Size written to %s does not match expected: %zd != %" PRIu32 ": %d (%s)", checkra1n_path, didWrite_checkra1n, checkra1n_len, errno, strerror(errno));
 		close(fd_checkra1n);
 		unlink(checkra1n_path);
 		free(checkra1n_path);
@@ -106,7 +107,7 @@ int exec_checkra1n(void) {
 			}
 			ssize_t didWrite_libcheckra1nhelper = write(helper_fd, libcheckra1nhelper_dylib, libcheckra1nhelper_dylib_len);
 			if ((unsigned int)didWrite_libcheckra1nhelper != libcheckra1nhelper_dylib_len) {
-				LOG(LOG_FATAL, "Size written does not match expected: %zd != %d: %d (%s)", didWrite_libcheckra1nhelper, libcheckra1nhelper_dylib_len, errno, strerror(errno));
+				LOG(LOG_FATAL, "Size written does not match expected: %zd != %" PRIu32 ": %d (%s)", didWrite_libcheckra1nhelper, libcheckra1nhelper_dylib_len, errno, strerror(errno));
 				close(helper_fd);
 				unlink(libcheckra1nhelper_dylib_path);
 				return -1;
